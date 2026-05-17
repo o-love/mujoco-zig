4863b6b763b3ad0fab36650f9d0a4ad516d7c479
Oscar Love <olabradalove@gmail.com>
2026-05-17 00:03:59 -0700

    Start setting up opengl-viewer example

--- examples/opengl-viewer/viewer.zig
+++ examples/opengl-viewer/viewer.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const rl = @import("raylib");
 
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
@@ -15,4 +16,29 @@ pub fn main(init: std.process.Init) !void {
     }
 
     std.log.info("{d} env vars", .{init.environ_map.count()});
+
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "mujoco-zig viewer example");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+        //----------------------------------------------------------------------------------
+    }
 }
