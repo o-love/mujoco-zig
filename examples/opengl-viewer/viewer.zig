e785a66f95a53e5276f9b720d0917b6912b2f5dc
Oscar Love <olabradalove@gmail.com>
2026-05-27 18:40:17 -0700

    Convert viewer debug to io

--- examples/opengl-viewer/viewer.zig
+++ examples/opengl-viewer/viewer.zig
@@ -20,41 +20,40 @@ pub fn main(init: std.process.Init) !void {
     _ = arg_it.skip();
     const model_path = arg_it.next() orelse "assets/hello.xml";
 
-    try std.Io.File.stdout().writeStreamingAll(io, "Initializing GLFW...\n");
+    const out = std.Io.File.stdout();
+    var buffer: [1024]u8 = undefined;
+    const writerStruct = out.writer(io, &buffer);
+    defer _ = writerStruct.flush();
+
+    const writer = writerStruct.interface;
+    try writer.print("Initializing GLFW...\n", .{});
 
     try glfw.init();
 
-    std.debug.print("glfw.init() returned successfully.\n", .{});
-    defer {
-        std.debug.print("Calling glfw.terminate()...\n", .{});
-        glfw.terminate();
-    }
+    try writer.print("glfw.init() returned successfully.\n", .{});
+    defer glfw.terminate();
 
-    std.debug.print("Setting error callback...\n", .{});
-
-    std.debug.print("Setting window hints...\n", .{});
-
-    std.debug.print("Creating window...\n", .{});
+    try writer.print("Creating window...\n", .{});
     const window: *glfw.Window = try glfw.createWindow(800, 640, "MuJoCo Zig Viewer", null, null);
     defer glfw.destroyWindow(window);
 
-    std.debug.print("Making context current...\n", .{});
+    try writer.print("Making context current...\n", .{});
     glfw.makeContextCurrent(window);
 
-    std.debug.print("Setting swap interval...\n", .{});
+    try writer.print("Setting swap interval...\n", .{});
     // VSync
     glfw.swapInterval(1);
 
-    std.debug.print("Window opened successfully.\n", .{});
+    try writer.print("Window opened successfully.\n", .{});
 
-    std.debug.print("Building model\n", .{});
+    try writer.print("Building model\n", .{});
     var model: MjModel = try .from_xml(model_path, init.gpa);
     defer model.deinit();
 
     var data: MjData = try model.data();
     defer data.deinit();
 
-    std.debug.print("Building context and scene\n", .{});
+    try writer.print("Building context and scene\n", .{});
 
     const options: MjvOption = .init();
 
